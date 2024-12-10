d66a4598fcfa92e0ad121c8c64dcac5f4e0772d0
Suene Souza <suene.vs@gmail.com>
2024-12-10 09:19:14 -0300

    first commit

---
+++ src/main/java/com/startdb/volunteerdb/VolunteerdbApplication.java
@@ -0,0 +1,13 @@
+package com.startdb.volunteerdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startdb/volunteerdb/VolunteerdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startdb.volunteerdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
