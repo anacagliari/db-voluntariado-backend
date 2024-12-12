ca4db988703c0d0ded1c223861e52afe49271d0a
Suene Souza <suene.vs@gmail.com>
2024-12-12 07:50:06 -0300

    add atributos

--- src/main/java/com/startdb/volunteerdb/model/Volunteer.java
+++ src/main/java/com/startdb/volunteerdb/model/Volunteer.java
@@ -1,12 +1,14 @@
 package com.startdb.volunteerdb.model;
 
 
+
 import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Volunteer {
@@ -14,20 +16,49 @@ public class Volunteer {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @NotNull(message = "O nome é obrigatório")
   private String name;
+
+  @NotNull(message = "O gênero é obrigatório")
+  private String gender;
+
+  @NotNull(message = "A idade é obrigatória")
+  private int age; 
+
+  @NotNull(message = "O cpf é obrigatório")
+  private String cpf;
+
+  @NotNull(message = "O telefone é obrigatório")
   private String phone;
+
+  @NotNull(message = "O e-mail é obrigatório")
   private String email;
+
+  @NotNull(message = "O cep é obrigatório")
+  private String cep;
+
+  @NotNull(message = "O logradouro é obrigatório")
   private String address;
+
+  @NotNull(message = "A cidde é obrigatório")
+  private String city;
+
+  @NotNull
   private List<String> areasDeSuporte;
 
 
- public Volunteer() {
+public Volunteer() {
   }
 
-  public Volunteer(String name, String phone, String email, String address, List<String> areasDeSuporte) {
+  public Volunteer(String name, String gender, int age, String cpf, String phone, String cep, String city, String email, String address, List<String> areasDeSuporte) {
     this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.cpf = cpf;
     this.phone = phone;
     this.email = email;
+    this.cep = cep;
+    this.city = city;
     this.address = address;
     this.areasDeSuporte = areasDeSuporte;
   }
@@ -42,6 +73,29 @@ public class Volunteer {
 
   public void setName(String name) {
     this.name = name;
+  }
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public int getAge( ) {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
   }
 
   public String getPhone() {
@@ -58,6 +112,22 @@ public class Volunteer {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getCep() {
+    return cep;
+  }
+
+  public void setCep(String cep) {
+    this.cep = cep;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
   }
 
   public String getAddress() {
